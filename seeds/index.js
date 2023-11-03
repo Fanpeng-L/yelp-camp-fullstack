f84efb0a0b0d1ed681654e5c4c0ba803f97cfdde
Fanpeng Li <serforfp@gmail.com>
2023-11-03 15:21:45 +0000

    Add styles

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://picsum.photos/400",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, quaerat? Similique, laboriosam dolores ea molestias, nam tenetur quidem iste possimus quae alias, dolorum autem. Modi laboriosam deserunt eligendi dolorem earum?",
+      price,
     });
     await camp.save();
   }
